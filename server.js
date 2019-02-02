9b0f0aa26d38124aeddd33d41d3e0aeb8fd04316
ujjwal0501 <31813384+Ujjwal0501@users.noreply.github.com>
2019-02-03 01:03:09 +0530

    install dependencies

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
-var mongoose = require('mongoose');
+var mysql = require('mysql');
 var express = require('express');
 var hbs = require('hbs');
 var bodyParser = require('body-parser');
 
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'hallucinators',
+  password: 'hallucinators',
+  database: 'hallucinators'
+})
 
-mongoose.connect("mongodb://localhost:27017/dotslash';")
+connection.connect(function(err) {
+  if (err) throw err
+  console.log('You are now connected...')
+})
 
  var app = express();
  app.set('view engine','hbs');
