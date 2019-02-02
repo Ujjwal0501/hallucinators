5ca1104fcd1baaf1dacdd36492e366c7f6e5b49e
keshav goyal <keshavgoyal2014@gmail.com>
2019-02-03 04:14:01 +0530

    added toll_admin interface and login page for toll_admin

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 var express = require('express');
 var hbs = require('hbs');
 var bodyParser = require('body-parser');
-
+var jwt = require('jsonwebtoken');
+var bcryptjs = require('bcryptjs');
 
 mongoose.connect("mongodb://localhost:27017/dotslash';")
 
@@ -19,6 +20,28 @@ mongoose.connect("mongodb://localhost:27017/dotslash';")
      res.render('index.hbs',{
          amount:1000
      })
+ });
+
+app.get('/tolllogin',(req,res) => {
+    res.render('toll_login.hbs',{});
+})
+
+app.post('/login',(req,res) => {
+    id=req.body.toll_LoginId;
+    password=req.body.password;
+    bcrypt.genSalt(10,(err,salt) => {
+        bcrypt.hash(password,salt,(err,hash) => {
+            password=hash;
+            next();
+        });
+    });
+})
+
+ app.get('/toll',(req,res) => {
+     res.render('toll_index.hbs',{
+         number:"GJ-05-1996",
+         fee:"120"
+     });
  })
 
  app.listen(3000,(err,res) => {
